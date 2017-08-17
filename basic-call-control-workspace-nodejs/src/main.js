3430b681531d621a82ea47033525cded4ba6685b
maksimlysenko <Maxim.Lysenko@genesys.com>
2017-08-17 11:21:58 -0700

    Minor comments update

--- basic-call-control-workspace-nodejs/src/main.js
+++ basic-call-control-workspace-nodejs/src/main.js
@@ -22,13 +22,13 @@ async function main() {
   try {
     
     //region Register event handlers
-    // Register event handlers to get notifications of call and dn state changes and implement the automated sequence
+    //Register event handlers to get notifications of call and dn state changes and implement the automated sequence
     api.on('CallStateChanged', async msg => {
 	//endregion	
       let call = msg.call;
       switch (call.state) {
         //region Answer call
-        // When a ringing call is detected, answer it.
+        //When a ringing call is detected, answer it.
         case 'Ringing':
           console.log('Answering call...');
           await api.voice.answerCall(call.id);
@@ -36,7 +36,7 @@ async function main() {
 		//endregion  
 
         //region Handle established state
-        // The first time we see an established call it will be placed on hold. The second time, it is released.
+        //The first time we see an established call it will be placed on hold. The second time, it is released.
         case 'Established':
           if (!callHasBeenHeld) {
             console.log('Placing call on hold...');
@@ -50,7 +50,7 @@ async function main() {
 		//endregion  
 
         //region Handle held call
-        // When we see a held call, retrieve it
+        //When we see a held call, retrieve it
         case 'Held':
           console.log('Retrieving call...');
           await api.voice.retrieveCall(call.id);
@@ -58,7 +58,7 @@ async function main() {
 		//endregion  
 
         //region Handle released
-        // When we see a released call, set the agent state to ACW
+        //When we see a released call, set the agent state to ACW
         case 'Released':
           console.log('Setting agent notReady w/ ACW...');
           await api.voice.notReady('AfterCallWork');
@@ -70,7 +70,7 @@ async function main() {
     api.on('DnStateChanged', async msg => {
       let dn = msg.dn;
       //region Handle DN state change
-      // When the DN workMode changes to AfterCallWork the sequence is over and we can exit.
+      //When the DN workMode changes to AfterCallWork the sequence is over and we can exit.
       console.log(`Dn updated - number [${dn.number}] state [${dn.agentState}] workMode [${dn.agentWorkMode}]...`);
       if (dn.agentWorkMode === 'AfterCallWork') {
         await api.destroy();
@@ -79,7 +79,7 @@ async function main() {
     });
 
     //region Initialize the API and activate channels
-    // Initialize the API and activate channels
+    //Initialize the API and activate channels
     console.log('Initializing API...');
     await api.initialize();
     console.log('Activating channels...');
@@ -87,7 +87,7 @@ async function main() {
 	//endregion
 
     //region Wait for an inbound call
-    // The sample waits and reacts to an inbound call to perform the automated sequence.
+    //The sample waits and reacts to an inbound call to perform the automated sequence.
     console.log('Waiting for an inbound call...');
   } catch (e) {
     await api.destroy();
