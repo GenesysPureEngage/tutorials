b026063c04b7a5689b76e6317fdd6a60d53e00d7
miremnev <Mikhail.Remnev@genesys.com>
2017-08-30 16:54:16 +0300

    init workspace with tokens

---
+++ initialize-workspace-nodejs/src/main.js
@@ -0,0 +1,47 @@
+const workspace = require('genesys-workspace-client-js');
+const authorization = require('genesys-authorization-client-js');
+
+const apiKey = "<apiKey>";
+const apiUrl = "<apiUrl>";
+
+//region Create the api object
+//Create the api object passing the parsed command line arguments.
+const workspaceApi = new workspace(apiKey, apiUrl);
+//endregion
+
+const client = new authorization.ApiClient();
+client.basePath = `${apiUrl}/auth/v3`;
+client.defaultHeaders = {'x-api-key': apiKey};
+client.enableCookies = true;
+const authApi = new authorization.AuthenticationApi(client);
+
+const agentUsername = "<agentUsername>";
+const agentPassword = "<agentPassword>";
+const clientId = "<clientId>";
+const clientSecret = "<clientSecret>";
+
+const api = new authorization.AuthenticationApi(client);
+const opts = {
+    authorization: "Basic " + new Buffer(`${clientId}:${clientSecret}`).toString("base64"),
+    clientId: clientId,
+    scope: '*',
+    username: agentUsername,
+    password: agentPassword
+};
+    
+api.retrieveTokenWithHttpInfo("password", opts).then(resp => {
+    const data = resp.response.body;
+    const accessToken = data.access_token;
+    if(!accessToken) {
+        throw new Error('Cannot get access token');
+    }
+    
+    return accessToken;
+}).then(token => {
+    //region Initiaize the API and activate channels
+    //Initialize the API and activate channels
+    workspaceApi.initialize({token: token}).then(data => {
+        console.log(data);
+        workspaceApi.destroy();
+    });
+}).catch(console.error);
