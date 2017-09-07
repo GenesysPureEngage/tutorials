0b0699639586dd820b98c036e01473493d35c532
miremnev <Mikhail.Remnev@genesys.com>
2017-09-07 17:43:51 +0300

    targets with tokens

--- targets-workspace-java/src/main/java/Main.java
+++ targets-workspace-java/src/main/java/Main.java
@@ -1,149 +1,62 @@
 import com.genesys.workspace.WorkspaceApi;
-import com.genesys.workspace.common.WorkspaceApiException;
-
-import com.genesys.workspace.events.CallStateChanged;
-import com.genesys.workspace.events.DnStateChanged;
 import com.genesys.workspace.models.User;
-import com.genesys.workspace.models.Call;
-import com.genesys.workspace.models.CallState;
-import com.genesys.workspace.models.AgentWorkMode;
-import com.genesys.workspace.models.Dn;
 import com.genesys.workspace.models.targets.TargetSearchResult;
-import com.genesys.workspace.models.targets.Target;
-import com.genesys.workspace.models.targets.TargetType;
 
 import com.genesys.internal.authorization.api.AuthenticationApi;
+import com.genesys.internal.authorization.model.DefaultOAuth2AccessToken;
 import com.genesys.internal.common.ApiClient;
-import com.genesys.internal.common.ApiResponse;
-import com.genesys.internal.common.ApiException;
 
-import java.util.Arrays;
-import java.util.Map;
 import java.util.Base64;
-import java.util.stream.Collectors;
 import java.util.concurrent.CompletableFuture;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		try {
-			//region Parsing options
-			//Here we parse the input params (ex: --baseUrl=<base url>)
+	static CompletableFuture future = new CompletableFuture();
+        
+	public static void main(String[] args) throws Exception {
+            //region creating WorkspaceApi
+            //Creating a WorkspaceApi object with the apiKey, baseUrl and 'debugEnabled' preference.
+            String apiKey = "<apiKey>";
+            String apiUrl = "<apiUrl>";
+
+            //region creating WorkspaceApi
+            //Creating a WorkspaceApi object with the apiKey, baseUrl and 'debugEnabled' preference.
+            WorkspaceApi api = new WorkspaceApi(apiKey, apiUrl, false);
+            //endregion
+
+            String authUrl = String.format("%s/auth/v3", apiUrl);
+            ApiClient authClient = new ApiClient();
+            authClient.setBasePath(authUrl);
+            authClient.addDefaultHeader("x-api-key", apiKey);
+            authClient.getHttpClient().setFollowRedirects(false);
+
+            AuthenticationApi authApi = new AuthenticationApi(authClient); 
+
+            String agentUsername = "<agentUsername>";
+            String agentPassword = "<agentPassword>";
+            String clientId = "<clientId>";
+            String clientSecret = "<clientSecret>";
+
+            String authorization = "Basic " + new String(Base64.getEncoder().encode(String.format("%s:%s", clientId, clientSecret).getBytes()));
+            DefaultOAuth2AccessToken resp = authApi.retrieveToken("password", authorization, "application/json", "*", clientId, agentUsername, agentPassword);
+
+            User user = api.initialize(resp.getAccessToken()).get();
+            api.activateChannels(user.getAgentId(), user.getAgentId());
+            api.voice().setAgentReady();
 			
-			final Map<String, String> options = Arrays.stream(args).filter(arg -> arg.startsWith("--"))
-				.map(arg -> arg.substring(2).split("=",2))
-				.collect(Collectors.toMap(arg -> arg[0].trim(), arg -> arg[1].trim()));
-			if(!options.containsKey("debugEnabled")) options.put("debugEnabled", "false");
-			
-			new Main(options);
-			//endregion
-			
-		} catch(Exception e) {
-			System.err.println("InvalcallId args");
-			System.err.println(e);
-		}
-		
+            System.out.println("Searching for targets");
+            TargetSearchResult result = api.targets().search("<searchTerm>");
+            if(result.getTotalMatches() > 0) {
+                result.getTargets().forEach(target -> {
+                    System.out.println("Name: " + target.getName());
+                    System.out.println("PhoneNumber: " + target.getNumber());
+                });
+            }
+            else {
+                    System.out.println("Search came up empty");
+            }
+
+            System.out.println("done");
+            api.destroy();
 	}
-	
-	boolean hasCalledInitiateConference = false;
-	boolean hasCalledCompleteConference = false;
-	
-	int actionsCompleted = 0;
-	
-	String consultConnId = null;
-	String parentConnId = null;
-	CompletableFuture future = new CompletableFuture();
-	WorkspaceApi api;
-	
-	
-	public Main(Map<String, String> options) {
-		//region creating WorkspaceApi
-		//Creating a WorkspaceApi object with the apiKey, baseUrl and 'debugEnabled' preference.
-		api = new WorkspaceApi(
-				options.get("apiKey"),
-				options.get("baseUrl"),
-				Boolean.parseBoolean(options.get("debugEnabled"))
-		);
-		//endregion
-		
-		try {
-			System.out.println("Getting auth code...");
-			String authCode = getAuthCode(
-				options.get("baseUrl"), 
-				options.get("apiKey"),
-				options.get("clientId"),
-				options.get("username"),
-				options.get("password")
-			);
-			if(Boolean.parseBoolean(options.get("debugEnabled"))) 
-				System.out.println("Auth code is: [" + authCode + "]");
-			
-			System.out.println("Initializing API...");
-			User user =  api.initialize(authCode, "http://localhost").get();
-			
-			System.out.println("Activating channels...");
-			api.activateChannels(user.getAgentId(), user.getAgentId());
-			api.voice().setAgentReady();
-			
-			System.out.println("Searching for targets");
-			TargetSearchResult result = api.targets().search(options.get("searchTerm"));
-			if(result.getTotalMatches() > 0) {
-				try {
-					Target target = result.getTargets().stream()
-						.filter(t -> t.getType() == TargetType.AGENT).findFirst().get();
-					System.out.println("Found target: " + target.getName());
-					System.out.println("Calling number: " + target.getNumber());
-					api.voice().makeCall(target.getNumber());
-					
-				} catch(Exception ex) {
-					System.out.println("No targets are agents");
-				}
-			} else {
-				System.out.println("Search came up empty");
-			}
-			
-			
-			
-			System.out.println("done");
-			api.destroy();
-			
-		} catch(Exception ex) {
-			System.err.println("Error: " + ex);
-			
-			try {
-				api.destroy();
-			} catch(WorkspaceApiException destroyEx) {
-				System.err.println("Could not destroy API: " + destroyEx);
-				System.exit(1);
-			}
-		}
-		
-	}
-	
-	public static String getAuthCode(String baseUrl, String apiKey, String clientId, String username, String password) throws ApiException {
-		
-		final ApiClient authClient = new ApiClient();
-		
-		authClient.setBasePath(baseUrl + "/auth/v3");
-		authClient.addDefaultHeader("x-api-key", apiKey);
-		authClient.getHttpClient().setFollowRedirects(false);
-		
-		final AuthenticationApi authApi = new AuthenticationApi(authClient); 
-		
-		final String authorization = "Basic " + new String(Base64.getEncoder().encode( (username + ":" + password).getBytes()));
-		
-		try {
-			final ApiResponse<Void> response = authApi.authorizeWithHttpInfo("code", "http://localhost", clientId, authorization, null);
-		} catch(ApiException ex) {
-			String location = ex.getResponseHeaders().get("Location").get(0);
-			String code = Arrays.stream(location.split("\\?")[1].split("&")).filter(q -> q.startsWith("code=")).findFirst().get().split("=")[1];
-			return code;
-			
-		}
-		
-		
-		return null;
-	}
-	
 }
 
