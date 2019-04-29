b5905bfa15be7c2a235b1bed6055816541cf37d5
Jean-Marc Taillant <jean-marc.taillant@genesys.com>
2019-04-29 13:10:17 +0200

    Move tutorial to new SDK

--- request-click-to-call-in-engagement-nodejs/src/main.js
+++ request-click-to-call-in-engagement-nodejs/src/main.js
@@ -1,16 +1,21 @@
-const request = require('request-promise');
-//region Edit constants
-// Start by editing the sample's constants:
+const EngagementService = require('engagement-client-js');
+
+//region Edit the sample’s constants:
+
 // API_BASEPATH is the base URL used to access PureEngage Cloud APIs.
-// GROUP_NAME is the name of the Click-To-Call-In configuration group. The Click-To-Call-In configuration 
-// is created in Platform Administrator.
+const API_BASEPATH = '<API Base path. For example: http://localhost:3005>';
+
 // API_KEY is the API key provided by Genesys that you must use with all the requests
 // to PureEngage Cloud APIs.
-const API_BASEPATH = '<API Base path. For example: http://localhost:3005>';
 const API_KEY = '<API Key>';
+
+// GROUP_NAME is the name of the Click-To-Call-In configuration group. The Click-To-Call-In configuration 
+// is created in Platform Administrator.
 const GROUP_NAME = '<Group name for Click-To-Call-In>';
+
+// FROM_PHONE_NUMBER is the phone number associated with the request.
 const FROM_PHONE_NUMBER = '<The optional customer phone number including the area code>';
-const CLICK_TO_CALL_IN_API_PATH = '/engagement/v3/call-in/requests/create';
+
 //endregion
 
 async function requestClickToCallIn() {
@@ -22,31 +27,21 @@ async function requestClickToCallIn() {
     // The optional parameters 'fromPhoneNumber' and 'userData' can be used to provide 
     // additional information.
     // The detailed description of all the parameters is available in the Click-To-Call-In API Reference.
-    let bodyData = JSON.stringify({
-        'groupName': GROUP_NAME,
-        'fromPhoneNumber': FROM_PHONE_NUMBER
-    });
-    //endregion
-
-    //region Create the Click-To-Call-In request options.
-    // Now that you have created the body of your Click-To-Call-In request, you can create the options
-    // that will be sent as part of the request. These options may vary slightly according to the
-    // client module that is used to handle the HTTP request (request-promise in this example).
-    // In the options, specify the API URL, the method as POST, the header parameters,
-    // and the request body that you created in the first step. 
-    // The API_BASEPATH constant used here is the base URL of the PureEngage Cloud APIs.
-    // The header parameter 'Content-Type' must be 'application/json'.
-    // The header parameter 'x-api-key' is the API key provided by Genesys and which
-    // must be used in all the requests to the PureEngage Cloud APIs.
-    const requestOptions = {
-        uri: API_BASEPATH + CLICK_TO_CALL_IN_API_PATH,
-        method: 'POST',
-        body: bodyData,
-        headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': API_KEY
-        }
+    var requestClientToCallIn = new EngagementService.CallInRequestsParms();
+    //requestClientToCallIn.groupName = GROUP_NAME;
+    requestClientToCallIn.fromPhoneNumber = FROM_PHONE_NUMBER;
+    requestClientToCallIn.groupName =GROUP_NAME;
+    requestClickToCallIn.userData = {
+        "priority": "high",
+        "email": "customer@email.com"
     };
+    //endregion
+  
+
+    //region Initialize the new CallbacksApi class instance.
+    var callInApi = new EngagementService.CallInApi();
+    callInApi.apiClient.basePath = API_BASEPATH;
+    callInApi.apiClient.enableCookies = false
     //endregion
 
     //region Send the request
@@ -59,13 +54,7 @@ async function requestClickToCallIn() {
     // the optional 'fromPhoneNumber' parameter.
     // Congratulations, you are done!
     try {
-        let clickToCallInResponse = await request(requestOptions);
-        if (!clickToCallInResponse) {
-            console.log('Invalid null or undefined response.');
-            return;
-        }
-
-        clickToCallInResponse = JSON.parse(clickToCallInResponse);
+        let clickToCallInResponse = await callInApi.callInRequests(API_KEY, requestClientToCallIn);
         console.log('Request status corrId : ' + clickToCallInResponse.status.corrId);
         console.log('Successfully created Click-To-Call-In request with id : ' + clickToCallInResponse.data.id + 
                         ', fromPhoneNumber : ' + clickToCallInResponse.data.fromPhoneNumber + 
