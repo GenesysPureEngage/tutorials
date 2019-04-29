fab95ef8973e06d9251c72a0b008799ad6514efe
Jean-Marc Taillant <jean-marc.taillant@genesys.com>
2019-04-29 11:45:01 +0200

    Use Engagement Nodejs sdk for this tutorial

--- get-business-hours-engagement-nodejs/src/main.js
+++ get-business-hours-engagement-nodejs/src/main.js
@@ -1,52 +1,36 @@
-const request = require('request-promise');
-//region Edit constants
-// Note:You must provision a Callback execution service and an associated Business Hours 
-// service in Designer.
-// Start by editing the sample's constants:
-// API_BASEPATH is the base URL used to access PureEngage Cloud APIs. 
-// API_KEY is the API key provided by Genesys that you must use with all the requests
-// to PureEngage Cloud APIs.
-const API_BASEPATH = '<API Base path. For example, http://localhost:8080>';
-const API_KEY = '<API Key>';
-const BUSINESSHOURS_SERVICE = '<Business hours service name>';
-const OPENFOR_API_PATH =  '/engagement/v3/callbacks/open-for/' + BUSINESSHOURS_SERVICE;
+const EngagementService = require('engagement-client-js');
+
+//region Edit the sample’s constants:
+
+// API_KEY is the API key provided by Genesys that you must use with all the requests // to PureEngage Cloud APIs.
+const API_KEY = "API_KEY"; 
+
+// API_BASEPATH is the base URL used to access PureEngage Cloud APIs.
+const API_BASEPATH = "API_BASEPATH";
+
+// BUSINESSHOURS_SERVICE is the name of the Business hours service that you need to provision in Designer.
+const BUSINESSHOURS_SERVICE = "BUSINESSHOURS_SERVICE";
 //endregion
 
 async function getBusinessHoursOpenFor() {
 
-    //region Create the request options.
-    // Create the options that are sent as part of the request. These options might vary 
-    // slightly according to the client module that is used to handle the HTTP request 
-    // (request-promise in this example).
-    // In the options, specify the API URL, the method as GET, and the header parameters.
-    // The Business Hours service used to query open hours is part of the URL.
-    // The header parameter 'Content-Type' must be 'application/json'.
-    // The header parameter 'x-api-key' is the API key provided by Genesys to use with all the 
-    // requests to PureEngage Cloud APIs. 
-    // Additional parameters are available to create a callback. You can find the list of 
-    // these optional parameters and detailed descriptions in the Business Hours API Reference.
-    const options = {
-      method: `GET`,
-      uri: API_BASEPATH + OPENFOR_API_PATH,
-      headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': API_KEY
-      },
-      json: true // Automatically parses the JSON string in the response
-    };
+     //region Initialize the new AvailabilityApi class instance.
+    var availabilityApi = new EngagementService.AvailabilityApi();
+    availabilityApi.apiClient.basePath = API_BASEPATH;
+    availabilityApi.apiClient.enableCookies = false
     //endregion
 
     //region Send the request
     // Send the request and parse the results.
     // Congratulations, you are done!
     try {
-        const response = await request(options);
-        if (!response) {
-            console.log('Invalid null or undefined response.');
-            return;
-        }
+        //See https://developer.genhtcc.com/reference/engagement/Availability/ for possible options
+        var opts={
+        };
+        const response = await availabilityApi.openFor(API_KEY, BUSINESSHOURS_SERVICE, opts);
         console.log('Request status corrId : ' + response.status.corrId );
         console.log('Business is open for : ' + response.data.openFor + ' seconds' );
+        
     }
     catch (error) {
         console.log('Failed to get business hours. Error : ' + error);
