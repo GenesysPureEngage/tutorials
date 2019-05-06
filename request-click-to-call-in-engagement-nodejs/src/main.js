f347f6ac5a76a83fe26a239bdae7ba47014b9c94
Jean-Marc Taillant <jean-marc.taillant@genesys.com>
2019-05-06 14:38:40 +0200

    Fix comments

--- request-click-to-call-in-engagement-nodejs/src/main.js
+++ request-click-to-call-in-engagement-nodejs/src/main.js
@@ -2,18 +2,18 @@ const EngagementService = require('engagement-client-js');
 
 //region Edit the sample’s constants:
 
-// API_BASEPATH is the base URL used to access PureEngage Cloud APIs.
+//API_BASEPATH is the base URL used to access PureEngage Cloud APIs.
 const API_BASEPATH = '<API Base path. For example: http://localhost:3005>';
 
-// API_KEY is the API key provided by Genesys that you must use with all the requests
-// to PureEngage Cloud APIs.
+//API_KEY is the API key provided by Genesys that you must use with all the requests
+//to PureEngage Cloud APIs.
 const API_KEY = '<API Key>';
 
-// GROUP_NAME is the name of the Click-To-Call-In configuration group. The Click-To-Call-In configuration 
-// is created in Platform Administrator.
+//GROUP_NAME is the name of the Click-To-Call-In configuration group. The Click-To-Call-In configuration 
+//is created in Platform Administrator.
 const GROUP_NAME = '<Group name for Click-To-Call-In>';
 
-// FROM_PHONE_NUMBER is the phone number associated with the request.
+//FROM_PHONE_NUMBER is the phone number associated with the request.
 const FROM_PHONE_NUMBER = '<The optional customer phone number including the area code>';
 
 //endregion
@@ -21,12 +21,12 @@ const FROM_PHONE_NUMBER = '<The optional customer phone number including the are
 async function requestClickToCallIn() {
 
     //region Create the Click-To-Call-In request body.
-    // First, you need to create the request body that is sent as part of the
-    // HTTP POST request. The 'groupName' parameter is mandatory.
-    // Tip: This group name is the name of Click-To-Call-In configuration in Platform Administrator.
-    // The optional parameters 'fromPhoneNumber' and 'userData' can be used to provide 
-    // additional information.
-    // The detailed description of all the parameters is available in the Click-To-Call-In API Reference.
+    //First, you need to create the request body that is sent as part of the
+    //HTTP POST request. The 'groupName' parameter is mandatory.
+    //Tip: This group name is the name of Click-To-Call-In configuration in Platform Administrator.
+    //The optional parameters 'fromPhoneNumber' and 'userData' can be used to provide 
+    //additional information.
+    //The detailed description of all the parameters is available in the Click-To-Call-In API Reference.
     var requestClientToCallIn = new EngagementService.CallInRequestsParms();
     requestClientToCallIn.fromPhoneNumber = FROM_PHONE_NUMBER;
     requestClientToCallIn.groupName =GROUP_NAME;
@@ -44,14 +44,14 @@ async function requestClickToCallIn() {
     //endregion
 
     //region Send the request
-    // Send the request and parse the results. The user can call the 
-    // phone number ( 'toPhoneNumber' property ) in the response before the 
-    // expiration time ( 'expirationTime' property ) and type in the access 
-    // code ( 'accessCode' property ). The access code is available  only 
-    // if the feature is enabled in the configuration group. Similarly, the 
-    // 'fromPhoneNumber' property is available only if the request contained 
-    // the optional 'fromPhoneNumber' parameter.
-    // Congratulations, you are done!
+    //Send the request and parse the results. The user can call the 
+    //phone number ( 'toPhoneNumber' property ) in the response before the 
+    //expiration time ( 'expirationTime' property ) and type in the access 
+    //code ( 'accessCode' property ). The access code is available  only 
+    //if the feature is enabled in the configuration group. Similarly, the 
+    //'fromPhoneNumber' property is available only if the request contained 
+    //the optional 'fromPhoneNumber' parameter.
+    //Congratulations, you are done!
     try {
         let clickToCallInResponse = await callInApi.callInRequests(API_KEY, requestClientToCallIn);
         console.log('Request status corrId : ' + clickToCallInResponse.status.corrId);
