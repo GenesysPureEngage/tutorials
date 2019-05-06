2140e9dc2c5ebbc03086da7de3d002a98991cd16
Jean-Marc Taillant <jean-marc.taillant@genesys.com>
2019-05-06 14:37:59 +0200

    Fix comments

--- get-ewt-engagement-nodejs/src/main.js
+++ get-ewt-engagement-nodejs/src/main.js
@@ -2,13 +2,13 @@ const EngagementService = require('engagement-client-js');
 
 //region Edit the sample’s constants:
 
-// API_KEY is the API key provided by Genesys that you must use with all the requests // to PureEngage Cloud APIs.
+//API_KEY is the API key provided by Genesys that you must use with all the requests // to PureEngage Cloud APIs.
 var API_KEY = "API_KEY"; 
 
-// API_BASEPATH is the base URL used to access PureEngage Cloud APIs.
+//API_BASEPATH is the base URL used to access PureEngage Cloud APIs.
 var API_BASEPATH = "API_BASEPATH";
 
-// COMMA_SEPARATED_VQ_NAMES is the comma separated list of the Virtual Queue names for which Estimated Wait Time (EWT) is required.
+//COMMA_SEPARATED_VQ_NAMES is the comma separated list of the Virtual Queue names for which Estimated Wait Time (EWT) is required.
 var COMMA_SEPARATED_VQ_NAMES = '<Comma separated Virtual Queue (VQ) names>';
 
 //endregion
@@ -21,15 +21,15 @@ async function getEstimatedWaitTime() {
     //endregion
 
     //region Send the request
-    // Send the request and parse the results.
-    // Congratulations, you are done!
+    //Send the request and parse the results.
+    //Congratulations, you are done!
     try {
         var opts={mode: 'mode1'}
         const response = await estimatedWaitTimeApi.estimatedWaitTimeAPI1(API_KEY, COMMA_SEPARATED_VQ_NAMES, opts) 
         console.log('Request status corrId : ' + response.status.corrId );
         for(var i =0; i < response.data.length; i++){
-            // If there is a problem getting the Estimated Wait Time for a Virtual Queue then -1 is returned as the value of 'estimatedWaitTime' property.
-            // The 'message' property provides information about the error.
+            //If there is a problem getting the Estimated Wait Time for a Virtual Queue then -1 is returned as the value of 'estimatedWaitTime' property.
+            //The 'message' property provides information about the error.
             if( response.data[i].estimatedWaitTime >= 0 ) {
                 console.log('Response item index ' + i + ', Virtual Queue : ' + response.data[i].virtualQueue + ', Estimated Wait Time (EWT) in seconds : ' + response.data[i].estimatedWaitTime );
             } else {
